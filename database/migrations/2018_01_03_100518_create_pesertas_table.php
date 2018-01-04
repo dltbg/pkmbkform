b3d767bd59c62d0e276ee91da4e155250933d9af
dltbg <dtobing81@gmail.com>
2018-01-04 14:44:41 +0700

    Migrate udah, tagar form udah

--- database/migrations/2018_01_03_100518_create_pesertas_table.php
+++ database/migrations/2018_01_03_100518_create_pesertas_table.php
@@ -15,6 +15,15 @@ class CreatePesertasTable extends Migration
     {
         Schema::create('pesertas', function (Blueprint $table) {
             $table->increments('id');
+            $table->char('nama_peserta', 50);
+            $table->string('nrp', 13);
+            $table->string('departemen', 50);
+            $table->char('nama_pkk', 50);
+            $table->string('alergi', 150);
+            $table->string('riwayat_penyakit', 200);
+            $table->string('no_hp', 12);
+            $table->string('no_hp_ortu', 12);
+            $table->string("id_line", 25);
             $table->timestamps();
         });
     }
--- database/migrations/2018_01_03_100531_create_absensis_table.php
+++ database/migrations/2018_01_03_100531_create_absensis_table.php
@@ -15,6 +15,17 @@ class CreateAbsensisTable extends Migration
     {
         Schema::create('absensis', function (Blueprint $table) {
             $table->increments('id');
+            $table->tinyInteger('bus_berangkat');
+            $table->tinyInteger('opening');
+            $table->tinyInteger('sesi_1');
+            $table->tinyInteger('sesi_2');
+            $table->tinyInteger('sesi_3');
+            $table->tinyInteger('sesi_4');
+            $table->tinyInteger('sesi_5');
+            $table->tinyInteger('sesi_6');
+            $table->tinyInteger('sesi_7');
+            $table->tinyInteger('closing'); 
+            $table->tinyInteger('bus_pulang');
             $table->timestamps();
         });
     }
